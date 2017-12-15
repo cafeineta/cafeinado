2d5eb4001b7dfd949922295433e563347ad6deb2
Lolillo <Lolillo@192.168.1.128>
2017-12-15 02:21:20 +0100

    practica opcional manuel jesus garcia

---
+++ practidagit/src/practicaed.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lolillo
+ */
+public class practicaed {
+    private String practica = "manuel jesus garcia";
+}
